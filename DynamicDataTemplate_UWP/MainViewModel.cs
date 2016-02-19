5d4bfeb33146b96f5b969906f1c365047daf84ce
Kyriacos Kyriacou <kyriacos.s.kyriacou@gmail.com>
2016-02-19 18:18:06 +0200

    Final commit

---
+++ DynamicDataTemplate_UWP/MainViewModel.cs
@@ -0,0 +1,42 @@
+﻿using DynamicDataTemplate_UWP.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace DynamicDataTemplate_UWP
+{
+    public class MainViewModel
+    {
+        public List<IModel> Coll { get; set; } = new List<IModel>
+        {
+            new ModelA {Title = "Model A" },
+            new ModelB {Title = "Model B" },
+        };
+    }
+
+    public class DynamicDataTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate ModelATemplate { get; set; }
+        public DataTemplate ModelBTemplate { get; set; }
+
+        protected override DataTemplate SelectTemplateCore(object item, DependencyObject container)
+        {
+            if (item is Models.ModelA)
+                return ModelATemplate;
+            else if (item is Models.ModelB)
+                return ModelBTemplate;
+            else return base.SelectTemplate(item, container);
+        }
+    }
+
+    namespace Models
+    {
+        public interface IModel { string Title { get; set; } }
+        public class ModelA : IModel { public string Title { get; set; } }
+        public class ModelB : IModel { public string Title { get; set; } }
+    }
+}
---
+++ DynamicDataTemplate_WPF/MainViewModel.cs
@@ -0,0 +1,25 @@
+﻿using DynamicDataTemplate_WPF.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicDataTemplate_WPF
+{
+    public class MainViewModel
+    {
+        public List<IModel> Coll { get; set; } = new List<IModel>
+        {
+            new ModelA {Title = "Model A" },
+            new ModelB {Title = "Model B" },
+        };
+    }
+
+    namespace Models
+    {
+        public interface IModel { string Title { get; set; } }
+        public class ModelA : IModel { public string Title { get; set; } }
+        public class ModelB : IModel { public string Title { get; set; } }
+    }
+}
